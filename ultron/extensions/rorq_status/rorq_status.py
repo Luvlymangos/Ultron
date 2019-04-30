469c740f85a436ebdf24eb3ee23b0793e7d6843e
BombaliKarpuz <vahapt@gmail.com>
2019-04-30 10:00:21 +0300

    Discord webhook support added

--- ultron/extensions/rorq_status/rorq_status.py
+++ ultron/extensions/rorq_status/rorq_status.py
@@ -4,6 +4,8 @@ from ultron.utils import make_embed
 from ultron.core import checks
 import discord
 import os
+import pytz
+from datetime import datetime
 
 class RorqStatus(commands.Cog):
     """This extension handles the time commands."""
@@ -32,20 +34,22 @@ class RorqStatus(commands.Cog):
             if rorqStatusChannel is None:
                 raise Exception('**FATAL ERROR** rorqstatus channel is not properly configured! Please contact system admin')
 
+            current_datetime = datetime.now(pytz.timezone('UTC')).strftime('%b %d %H:%M (EVE Time)')
+            last_update = "**Last Update** {}".format(current_datetime)
             if status == 'green':
                 current_folder = os.path.dirname(__file__)
-                filename = os.path.join(current_folder, 'siege_green.gif')
+                filename = os.path.join(current_folder, 'status_green.png')
                 file = discord.File(filename)
 
                 await self.delete_messages(rorqStatusChannel)
-                await rorqStatusChannel.send("Mine, mine, mine", file=file)
+                await rorqStatusChannel.send("@here {}".format(last_update), file=file)
             elif status == 'red':
                 current_folder = os.path.dirname(__file__)
-                filename = os.path.join(current_folder, 'siege_red.jpg')
+                filename = os.path.join(current_folder, 'status_red.png')
                 file = discord.File(filename)
 
                 await self.delete_messages(rorqStatusChannel)
-                await rorqStatusChannel.send("Siege Red girls and boys. No exceptions!", file=file)
+                await rorqStatusChannel.send("@here {}".format(last_update), file=file)
             else:
                 raise Exception('**ERROR:** Unknown status command {} Do !help rorqstatus for more info'.format(status))
         except Exception as e:
